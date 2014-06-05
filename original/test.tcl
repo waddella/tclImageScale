ec002d0c40022bc3d49bfa21e5da20fa215af3fb
Adrian Waddell <adrian@waddell.ch>
2014-06-05 13:33:15 -0400

    Added compiling instructions for OSX

--- original/test.tcl
+++ original/test.tcl
@@ -1,6 +1,12 @@
 # On Linux compile with
 # gcc -shared -o imgscale.so -I/usr/include/tcl imgscale.c -DUSE_TCL_STUBS -DUSE_TK_STUBS -ltclstub8.6 -ltkstub8.6 -fPIC
 
+# On OSX
+# gcc -shared -o imgscale.dylib -framework tcl  imgscale.c -DUSE_TCL_STUBS -DUSE_TK_STUBS -ltclstub8.6 -ltkstub8.6
+# tclsh8.6 test.tcl
+
+load [file join imgscale[info sharedlibextension]]
+
 set imgFrom [image create photo -file Tux.png]
 set imgTo [image create photo]
 
